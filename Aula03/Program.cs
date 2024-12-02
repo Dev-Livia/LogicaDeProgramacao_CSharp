cdd4b82f8c2058959eddf7ef45992301b070823c
Lívia <101743377+Dev-Livia@users.noreply.github.com>
2024-12-02 19:01:54 -0300

    Create Program.cs

---
+++ Aula03/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+namespace Aula03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+            string nome;
+            char sexo;
+            int idade;
+            double salario;
+
+            nome = "Lívia Nicole";
+            sexo = 'F';
+            idade = 20;
+
+
+        }
+    }
+}
