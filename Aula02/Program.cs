e465b5384ecb632fd61bdd149b8781bdee95e922
Lívia <101743377+Dev-Livia@users.noreply.github.com>
2024-12-02 15:35:27 -0300

    Update Program.cs

--- Aula02/Program.cs
+++ Aula02/Program.cs
@@ -10,6 +10,24 @@ namespace Aula02
     {
         static void Main(string[] args)
         {
+            int idade;
+            double salario, altura;
+            char genero;
+            string nome;
+
+            idade = 32;
+            salario = 4000.9;
+            altura = 1.72;
+            genero = 'F';
+            nome = "Lívia Nicole";
+
+            Console.WriteLine(idade);
+            Console.WriteLine(salario);
+            Console.WriteLine(altura);
+            Console.WriteLine(genero);
+            Console.WriteLine(idade);
+            Console.WriteLine(nome);
+
         }
     }
 }
