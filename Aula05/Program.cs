b49521ed9f18175231724fc46af2dfef0bbc79c6
Lívia <101743377+Dev-Livia@users.noreply.github.com>
2024-12-03 17:39:14 -0300

    Entrada de dados em C#

--- Aula05/Program.cs
+++ Aula05/Program.cs
@@ -10,6 +10,19 @@ namespace Aula05
     {
         static void Main(string[] args)
         {
+            string Nome1, Nome2;
+            int Idade1, Idade2;
+
+
+            Console.WriteLine("Digite o nome da PRIMEIRA pessoa: ");
+            Nome1 = Console.ReadLine();
+            Console.WriteLine("Digite a idade da PRIMEIRA pessoa: ");
+            Idade1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o nome da SEGUNDA pessoa: ");
+            Nome2 = Console.ReadLine();
+            Console.WriteLine("Digite a idade da SEGUNDA pessoa: ");
+            Idade2 = int.Parse(Console.ReadLine());
         }
     }
 }
