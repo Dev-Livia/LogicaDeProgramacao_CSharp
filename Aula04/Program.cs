68697b5ffe92a057b2e6ec6b981a7f82db32a230
Lívia <101743377+Dev-Livia@users.noreply.github.com>
2024-12-03 14:08:59 -0300

    Processamento e casting em C#

---
+++ Aula04/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Aula04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+            
+            double b1, b2, h, area;
+            b1 = 6.0;
+            b2 = 8.0;
+            h = 5.0;
+
+            area = (b1 + b2) / 2.0 * h;
+            Console.WriteLine(area.ToString("F2", CI));
+        }
+    }
+}
