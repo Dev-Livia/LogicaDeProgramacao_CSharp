47fb59b53d6bb0e6f8576e4cb071de1d24c06dbe
Lívia <101743377+Dev-Livia@users.noreply.github.com>
2024-12-05 17:05:39 -0300

    Solução do problema idades

---
+++ Aula08/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade1, idade2;
+            string nome1, nome2;
+            double media;
+
+            Console.WriteLine("INSIRA OS DADOS DO PRIMEIRO USUARIO");
+            Console.WriteLine();
+            Console.Write("NOME: "); nome1 = Console.ReadLine();
+            Console.Write("IDADE: "); idade1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("INSIRA OS DADOS DO SEGUNDO USUARIO");
+            Console.WriteLine();
+            Console.Write("NOME: "); nome2 = Console.ReadLine();
+            Console.Write("IDADE: "); idade2 = int.Parse(Console.ReadLine());
+
+            media = (idade1 + idade2) / 2;
+
+            Console.WriteLine("A idade media de "+nome1+" e "+ nome2 +" e de  " + media);
+        }
+    }
+}
